7e441eb85fd85000fc4fbb63746c1c47f6e21d58
botir-nematilloyev <nematilloyevbro@gmail.com>
2025-11-15 14:18:21 +0500

    Chapter3 finished

--- Chapter3/variables.go
+++ Chapter3/variables.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//	var name type = value
 	/*
@@ -38,4 +40,45 @@ func main() {
 	//x := "Botir"
 	//fmt.Println(x)
 
+	//Constants
+	//const MUSIC_PLAYER string = "KONSTA"
+	//fmt.Println(MUSIC_PLAYER)
+
+	// Defining multiple variables or constants
+	//var (
+	//	a = 5
+	//	b = 7
+	//	c = "hello"
+	//)
+	//fmt.Println(a, b, c)
+	//
+	//const (
+	//	d = 5
+	//	e = 7
+	//	f = "hello"
+	//)
+	//fmt.Println(d, e, f)
+
+	// Scanf usage
+	//fmt.Println("Son kiriting: ")
+	//var input float64
+	//fmt.Scanf("%f", &input)
+	//
+	//output := input * 2
+	//fmt.Println(output)
+
+	//Exercises
+	//#5
+	//var F float32
+	//fmt.Println("Farangentni kiriting: ")
+	//fmt.Scanf("%f", &F)
+	//Celsius := (F - 32) * 5 / 9
+	//fmt.Println(Celsius)
+
+	//#6
+	var metr float64
+	fmt.Println("Metrni kiriting: ")
+	fmt.Scanf("%f", &metr)
+	ft := metr * 0.3048
+	fmt.Println(ft)
 }
