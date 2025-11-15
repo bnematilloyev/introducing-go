8726d3170b9cbea5fc24c94471e2e213e82fd931
botir-nematilloyev <nematilloyevbro@gmail.com>
2025-11-15 16:03:54 +0500

    Chapter5 arrays

---
+++ Chapter5/arrays.go
@@ -0,0 +1,33 @@
+package main
+
+// built-in types arrays, slices and maps
+func main() {
+
+	//var arr [5]int
+	//arr[4] = 100
+	//fmt.Println("Array is", arr)
+
+	// Array usage
+	//var x int
+	//fmt.Println("Nechta son kiritmoqchisiz ?")
+	//fmt.Scanf("%d", &x)
+	//arr := make([]float32, x)
+	//var y float32
+	//var sum float32 = 0
+	//for i := 0; i < x; i++ {
+	//	fmt.Scanf("%f", &y)
+	//	arr[i] = y
+	//	sum += arr[i]
+	//}
+	//fmt.Println("Array:", arr)
+	//fmt.Println("Avearge:", sum/float32(len(arr)))
+
+	// Another way to create array and sum it with for loop
+	//arr := [5]float32{15, 48, 78, 96, 52}
+	//
+	//var total float32 = 0
+	//for _, value := range arr {
+	//	total += value
+	//}
+	//fmt.Println(total / float32(len(arr)))
+}
