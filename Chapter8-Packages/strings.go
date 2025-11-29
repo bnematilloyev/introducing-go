66f8cdc381393b9429f0573205a0a2c0ceec7bd8
botir-nematilloyev <nematilloyevbro@gmail.com>
2025-11-29 16:49:27 +0500

    Chapter8 Strings Package Functions

---
+++ Chapter8-Packages/strings.go
@@ -0,0 +1,81 @@
+package main
+
+func main() {
+	/*
+		// Contains(s, substr string) bool
+		fmt.Println(strings.Contains("Botir", "to"))
+		// false
+	*/
+
+	/*
+		//func Count(s, substr string) int
+		fmt.Println(strings.Count("testtesttesttesttest", "test"))
+		// 5
+	*/
+
+	/*
+		// func HasPrefix(s, prefix string) bool
+		// Eng tezkor va optimal funksiya qidirish uchun
+		fmt.Println(strings.HasPrefix("test", "te"))
+		// true
+	*/
+
+	/*
+		// func HasSuffix(s, suffix string) bool
+		fmt.Println(strings.HasSuffix("Botir", "ir"))
+		// true
+	*/
+
+	/*
+		// func Index(s, substr string) int
+		fmt.Println(strings.Index("TEST", "B"))
+		// -1 when not index
+		fmt.Println(strings.Index("TEST", "T"))
+		// returns first "T" index -> 0
+	*/
+
+	/*
+		// func Join(a []string, sep string) string
+		fmt.Println(strings.Join([]string{"#", "I", "Am", "Botir", "Nematilloyev"}, ""))
+		// #IAmBotirNematilloyev
+	*/
+
+	/*
+		// func Repeat(s string, count int) string
+		fmt.Println(strings.Repeat("King", 5))
+		//KingKingKingKingKing
+	*/
+
+	/*
+		// func Replace(s, old, new string, n int) string
+		fmt.Println(strings.Replace("DublaSeblaChorshanbaPayblaJuma", "bla", "shanba", 2))
+		// DushanbaSeshanbaChorshanbaPayblaJuma
+	*/
+
+	/*
+		// func Split(s, sep string) []string
+		fmt.Println(strings.Split("1,2,3,4,5,6,7", ","))
+		// [1 2 3 4 5 6 7]
+	*/
+
+	/*
+		// func ToLower(s string) string
+			fmt.Println(strings.ToLower("BOTIR"))
+		// botir
+	*/
+
+	/*
+		// func ToUpper(s string) string
+			fmt.Println(strings.ToUpper("botir"))
+		// BOTIR
+	*/
+
+	/*
+		// working with strings as binary data
+		arr := []byte("test")
+		str := string([]byte{'t', 'e', 's', 't'})
+		fmt.Println(arr, str)
+		// [116 101 115 116] test
+	*/
+
+}
